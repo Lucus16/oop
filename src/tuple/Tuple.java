ae577713e3b0b9e20213fc62f1447f4aae4c94f1
Sal Wolffs <sal.wolffs@gmail.com>
2014-02-28 23:36:57 +0100

    Created MinMaxAI stub

---
+++ src/tuple/Tuple.java
@@ -0,0 +1,31 @@
+package tuple;
+
+/**
+ * Provides a general implementation of a classic 2-member tuple.
+ * Intended as a near-primitive and as such not encapsulated.
+ * Has two public member fields, left and right, 
+ * not necessarily of the same type.
+ * @author Sal Wolffs(s4064542)
+ * @author Lars Jellema(s4388747)
+ *
+ */
+public class Tuple <L,R> {
+	public L left;
+	public R right;
+	/**
+	 * Initializes a tuple to given values.
+	 * @param left
+	 * @param right
+	 */
+	Tuple (L left, R right){
+		this.left=left;
+		this.right=right;
+	}
+	/**
+	 * Initializes both fields to null.
+	 */
+	Tuple (){
+		left=null;
+		right=null;
+	}
+}
