263b4ba740257f137b5576625a699c4b35259107
Lucus16 <lars.jellema@gmail.com>
2014-03-17 13:31:17 +0100

    Documented solver

--- src/hw6/Solver.java
+++ src/hw6/Solver.java
@@ -6,15 +6,18 @@ import java.util.*;
  * A class that implements a breadth-first search algorithm for finding the
  * Graphs for which the isGoal predicate holds
  * 
- * @author Pieter Koopman, Sjaak Smetsers
- * @version 1.3
- * @date 28-02-2013
+ * @author Lars Jellema s4388747
+ * @author Sal Wolffs s4064542
  */
 public class Solver {
 	// A queue for maintaining graphs that are not visited yet.
 	PriorityQueue<Node<Graph>> toExamine;
 	HashSet<Graph> examined;
 
+	/**
+	 * Create a new solver with starting Graph g.
+	 * @param g
+	 */
 	public Solver(Graph g) {
 		examined = new HashSet<Graph>();
 		toExamine = new PriorityQueue<Node<Graph>>();
@@ -22,21 +25,18 @@ public class Solver {
 	}
 
 	/**
-	 * A skeleton implementation of the solver
+	 * A solver using a priority queue.
 	 * 
 	 * @return a string representation of the solution
 	 */
 	public List<Graph> solve() {
 		while (!toExamine.isEmpty()) {
 			Node<Graph> next = toExamine.remove();
-			//System.out.println("Trying \n" + next.getItem().toString());
 			if (next.getItem().isGoal()) return next.path();
 			if (examined.contains(next.getItem())) {
-				//System.out.println("Already seen, skipping.");
 				continue;
 			}
 			for (Graph succ : next.getItem().successors()) {
-				//System.out.println("Adding \n" + succ.toString());
 				toExamine.add(new Node<Graph>(next,succ));
 			}
 			examined.add(next.getItem());
