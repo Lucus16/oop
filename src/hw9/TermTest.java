e7d987ba6cb8475bf6a92ae9811cde974952eb25
Sal Wolffs <sal.wolffs@gmail.com>
2014-05-12 01:15:57 +0200

    mostly finished writing unit tests for Term.

--- src/hw9/TermTest.java
+++ src/hw9/TermTest.java
@@ -16,7 +16,8 @@ import org.junit.Test;
  */
 public class TermTest {
 	
-	ArrayList<Term> termPool;
+	private ArrayList<Term> termPool;
+	private static double[] xPool = new double[]{0,0.01,1,1.6,2,2.72,3,3.14,4};
 	
 	@Before
 	public final void setup(){
@@ -26,9 +27,11 @@ public class TermTest {
 		termPool.add(new Term(3.14,1));
 		termPool.add(new Term(0,4));
 		termPool.add(new Term(2.72,0));
-		termPool.add(new Term(7.5,21321));
+		termPool.add(new Term(7.5,256));
 		termPool.add(new Term(-1, 3));
 		termPool.add(new Term(5,-2));
+		termPool.add(new Term(3,3));
+		termPool.add(new Term(325.57,1));
 	}
 	
 	/**
@@ -36,7 +39,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testTerm() {
-		assertEquals("Term() != Term(0,0)",new Term(),new Term(0,0));
+		assertEquals(new Term(),new Term(0,0));
 	}
 
 	/**
@@ -53,7 +56,7 @@ public class TermTest {
 	@Test
 	public final void testTermTerm() {
 		for (Term t : termPool){
-			assertEquals("Copied Term unequal to original",t,new Term(t));
+			assertEquals(t,new Term(t));
 		}
 	}
 
@@ -63,9 +66,9 @@ public class TermTest {
 	@Test
 	public final void testApply() {
 		for (Term t : termPool){
-			for (int i = 0; i<=4 ; ++i ){
+			for (double x : xPool){
 				assertEquals("Inaccurate evaluation on " + t.toString(),
-						t.getCoef()*Math.pow(i,t.getExp()),t.apply(i),0.001);
+						t.getCoef()*Math.pow(x,t.getExp()),t.apply(x),0.001);
 			}
 		}
 	}
@@ -75,7 +78,18 @@ public class TermTest {
 	 */
 	@Test
 	public final void testAdd() {
-		
+		for (Term t : termPool){
+			for (Term u : termPool){
+				if (t.getExp() == u.getExp()){
+					for (double x : xPool){
+						assertEquals("Evaluation should preserve addition",
+							t.apply(x)+u.apply(x),
+							t.add(u).apply(x),
+							0.001);
+					}
+				}
+			}
+		}
 	}
 
 	/**
@@ -83,7 +97,21 @@ public class TermTest {
 	 */
 	@Test
 	public final void testSub() {
-		fail("Not yet implemented"); // TODO
+		for (Term t : termPool){
+			for (Term u : termPool){
+				if (t.getExp() == u.getExp()){
+					for (double x : xPool){
+						assertEquals("Evaluation should preserve subtraction",
+							t.apply(x)-u.apply(x),
+							new Term(t).sub(u).apply(x),
+							0.001);
+						assertEquals("t+(-1)u==t-u",
+								new Term(t).sub(u),
+								new Term(t).add(u.mul(new Term(-1,0))));
+					}
+				}
+			}
+		}
 	}
 
 	/**
@@ -91,7 +119,25 @@ public class TermTest {
 	 */
 	@Test
 	public final void testMul() {
-		fail("Not yet implemented"); // TODO
+		for (Term t : termPool){
+			for (Term u : termPool){
+				for (double x : xPool){
+					if (x == 0 && (u.getExp() < 0 || t.getExp() < 0)){
+						continue; //cannot properly evaluate 0^-1
+					}
+					assertEquals(
+							"Evaluation should preserve multiplication for " +
+							t.toString() + " and " + u.toString() + " at x = " +
+							Double.toString(x) + " with intermediate results " +
+							Double.toString(t.apply(x)) + " and " + 
+							Double.toString(u.apply(x)),
+						(t.apply(x))*(u.apply(x)),
+						new Term(t).mul(u).apply(x),
+						Math.max(0.001, 
+								Math.abs((t.apply(x)*u.apply(x))/10000)));
+				}
+			}
+		}
 	}
 
 	/**
@@ -99,7 +145,28 @@ public class TermTest {
 	 */
 	@Test
 	public final void testDiv() {
-		fail("Not yet implemented"); // TODO
+		for (Term t : termPool){
+			for (Term u : termPool){
+				for (double x : xPool){
+					if (u.apply(x) == 0){
+						continue; //DIV0 on the expected value.
+					}
+					if (x == 0 && (u.getExp() < 0 || t.getExp() < 0)){
+						continue; //cannot properly evaluate 0^-1
+					}
+					assertEquals(
+							"Evaluation should preserve division for " +
+							t.toString() + " and " + u.toString() + " at x = " +
+							Double.toString(x) + " with intermediate results " +
+							Double.toString(t.apply(x)) + " and " + 
+							Double.toString(u.apply(x)),
+						t.apply(x)/u.apply(x),
+						new Term(t).div(u).apply(x),
+						Math.max(0.001, 
+								Math.abs((t.apply(x)/u.apply(x))/10000)));
+				}
+			}
+		}
 	}
 
 	/**
@@ -107,7 +174,9 @@ public class TermTest {
 	 */
 	@Test
 	public final void testNeg() {
-		fail("Not yet implemented"); // TODO
+		for (Term t : termPool){
+			assertEquals("-t should be (-1*x^0)*t",t.mul(new Term(-1,0)),t.neg());
+		}
 	}
 
 	/**
@@ -115,7 +184,17 @@ public class TermTest {
 	 */
 	@Test
 	public final void testPow() {
-		fail("Not yet implemented"); // TODO
+		for (Term t : termPool){
+			for (int i = 0; i < 10; ++i){
+				for (double x : xPool){
+					assertEquals("Evaluation should preserve exponentiation",
+							Math.pow(t.apply(x),i),
+							new Term(t).pow(i).apply(x),
+							Math.max(0.001, 
+									Math.abs(Math.pow(t.apply(x),i)/10000)));
+				}
+			}
+		}
 	}
 
 	/**
@@ -123,7 +202,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testGetCoef() {
-		fail("Not yet implemented"); // TODO
+		//trivial
 	}
 
 	/**
@@ -131,7 +210,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testSetCoef() {
-		fail("Not yet implemented"); // TODO
+		//trivial
 	}
 
 	/**
@@ -139,7 +218,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testGetExp() {
-		fail("Not yet implemented"); // TODO
+		//trivial
 	}
 
 	/**
@@ -147,7 +226,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testSetExp() {
-		fail("Not yet implemented"); // TODO
+		//trivial
 	}
 
 	/**
@@ -155,7 +234,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testScanTerm() {
-		fail("Not yet implemented"); // TODO
+		//given by assignment
 	}
 
 	/**
@@ -163,7 +242,20 @@ public class TermTest {
 	 */
 	@Test
 	public final void testCompareTo() {
-		fail("Not yet implemented"); // TODO
+		for(Term t : termPool){
+			for(Term u : termPool){
+				if(t.equals(u)){
+					assertTrue(t.compareTo(u) == 0);
+				}
+				if(t.compareTo(u) == 0)
+				{
+					assertTrue(t.equals(u));
+				}
+				if(t.getExp() < u.getExp()){
+					assertTrue(t.compareTo(u) < 0);
+				}
+			}
+		}
 	}
 
 	/**
@@ -171,7 +263,7 @@ public class TermTest {
 	 */
 	@Test
 	public final void testToString() {
-		fail("Not yet implemented"); // TODO
+		//human readability not autotestable. Handtested OK.
 	}
 
 }
