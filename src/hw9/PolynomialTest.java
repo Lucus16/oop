ad12eeb33e9f11aec24dbaa94e0438282ddcf45c
Sal Wolffs <sal.wolffs@gmail.com>
2014-05-11 22:40:52 +0200

    Generated JUnit test class for Polynomials by automagic.

---
+++ src/hw9/PolynomialTest.java
@@ -0,0 +1,168 @@
+/**
+ * 
+ */
+package hw9;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author sal
+ *
+ */
+public class PolynomialTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#Polynomial()}.
+	 */
+	@Test
+	public final void testPolynomial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#Polynomial(hw9.Term)}.
+	 */
+	@Test
+	public final void testPolynomialTerm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#Polynomial(hw9.Polynomial)}.
+	 */
+	@Test
+	public final void testPolynomialPolynomial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#add(hw9.Term)}.
+	 */
+	@Test
+	public final void testAddTerm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#add(hw9.Polynomial)}.
+	 */
+	@Test
+	public final void testAddPolynomial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#sub(hw9.Term)}.
+	 */
+	@Test
+	public final void testSubTerm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#sub(hw9.Polynomial)}.
+	 */
+	@Test
+	public final void testSubPolynomial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#mul(hw9.Term)}.
+	 */
+	@Test
+	public final void testMulTerm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#mul(hw9.Polynomial)}.
+	 */
+	@Test
+	public final void testMulPolynomial() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#div(hw9.Term)}.
+	 */
+	@Test
+	public final void testDiv() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#Polynomial(java.lang.String)}.
+	 */
+	@Test
+	public final void testPolynomialString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#clear()}.
+	 */
+	@Test
+	public final void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#apply(double)}.
+	 */
+	@Test
+	public final void testApply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link hw9.Polynomial#iterator()}.
+	 */
+	@Test
+	public final void testIterator() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
