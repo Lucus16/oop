eee35fc167b3282e989d1494626c05b50eb96623
Sal Wolffs <sal.wolffs@gmail.com>
2014-05-25 21:37:25 +0200

    Minor start on Crossing.

--- src/hw11/Crossing.java
+++ src/hw11/Crossing.java
@@ -8,5 +8,12 @@ package hw11;
  * English version of the Dutch "Regelaar"
  */
 class Crossing {
-
+    private static final int left = (RoadView.WINDOWSIZE / 2) - Car.CARWIDTH - 4;
+    private static final int width = 2 * Car.CARWIDTH + 8;
+    private Model model;
+    
+    public Crossing(Model m){
+    	model = m;
+    	
+    }
 }
